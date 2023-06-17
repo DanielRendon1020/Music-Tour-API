53e8928165f7ea9cfd6981f0823e8662d4f3a854
DanielRendon1020 <d.danny1020@gmail.com>
2023-06-17 12:36:49 -0700

    setup sequelize and connect to db

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const {Sequelize} = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+try{
+    sequelize.authenticate()
+    console.log('Connected 🐘')
+}catch(err){
+    console.log('🚫:', err)
+
+}
 
 // ROOT
 app.get('/', (req, res) => {
